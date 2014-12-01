c0435ea7068ef9572b035498a637d93af6f4181a
hebe889900 <jackyan63@gmail.com>
2014-12-01 16:44:30 -0500

    One more test case

--- test_mining.py
+++ test_mining.py
@@ -13,11 +13,16 @@ __status__ = "Prototype"
 # imports one per line
 from mining import *
 
-
+#Test if the function "six_best_months" and "six_worth_months" return the correct list.
 def test_goog():
     read_stock_data("GOOG", "data/GOOG.json")
     assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
                                  ('2008/05', 576.29), ('2008/06', 555.34)]
     assert six_worst_months() == [('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09), ('2004/12', 181.01),
                                   ('2005/03', 181.18), ('2005/01', 192.96)]
+
+#Test if the function insertitem() inserts the item into dictionary properly.
+def insertitem():
+    dic = {}
+    assert insertitem("2007/12",100,100,dic) == {"2007/12":(100,100)}
 
