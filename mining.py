d447325deee718ea8a4b9047642c05c600eda554
liutao3 <terryalice.liu@mail.utoronto.ca>
2014-12-01 20:24:38 -0500

    Main functions to data mining to calculate 6 best and 6 worst stock

--- mining.py
+++ mining.py
@@ -2,14 +2,6 @@
 
 """ Docstring """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
-
 # imports one per line
 import json
 import datetime
